c77b25e54998ea99c93f9251bc76cb29d31d16ed
scottzockoll <32425272+scottzockoll@users.noreply.github.com>
2020-03-19 17:38:35 -0400

    Reverted example

--- examples/geeks.cpp
+++ examples/geeks.cpp
@@ -1,54 +1,35 @@
-#include<iostream>
+// This is some random cpp file from https://www.geeksforgeeks.org/c-classes-and-objects/
+
+#include <bits/stdc++.h>
 using namespace std;
+class Geeks
+{
+    // Access specifier
+    public:
 
-#include"Service.h"
+    // Data Members
+    string geekname;
 
-Service * first = new Service();
-
-int getCommand(){
-    cout<<"Choose one of the following commands: "<<endl;
-    cout<<"1. Add new service"<<endl;
-    cout<<"2. Add new subservice"<<endl;
-    cout<<"3. Add parent to a service"<<endl;
-    cout<<"4. Delete a service(and it's subservices)"<<endl;
-    cout<<"Your Choice: ";
-    int c;
-    cin>>c;
-    return c;
-}
-void addService(){
-    first->add();
-}
-
-void addSubService(){
-    cout<<"Let's choose the parent first: "<<endl;
-    int * one = new int;
-    *one = 1;
-    first->print(one,0);
-    cout<<"0. here."<<endl<<"> ";
-    int id;
-    cin>>id;
-    Service * f = first->find(one,id);
-
-}
-
-void addParentToService(){
-
-}
-
-void doCommand(int c){
-    switch(c){
-    case 1:
-        addService();
-        break;
-    case 2:
-        addSubService();
-        break;
-    case 3:
-        addParentToService();
-        break;
-    case 4:
-        //deleteService();
-        break;
+    // Member Functions()
+    const void printName()
+    {
+       cout << "Geekname is: " << geekname;
     }
+
+    int doNothing(int arg1, double arg2) {
+        // this is just to test the parser
+    }
+};
+
+int main() {
+
+    // Declare an object of class geeks
+    Geeks obj1;
+
+    // accessing data member
+    obj1.geekname = "Abhi";
+
+    // accessing member function
+    obj1.printname();
+    return 0;
 }
