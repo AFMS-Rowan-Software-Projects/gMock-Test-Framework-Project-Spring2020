3fa91b9c8639533521d7e01dcd15b81441cc9508
nmichelette <43892144+nmichelette@users.noreply.github.com>
2020-03-23 05:39:09 -0400

    more testing

--- runner.py
+++ runner.py
@@ -2,20 +2,22 @@ import subprocess
 import platform
 
 
-
-def run_tests(testname = None, subtestname = None):
+def run_tests(filename, testname = None, subtestname = None):
     sys = platform.system()
+    process = None
     if sys == "Windows":
         print("Does not work on windows yet.")
     elif sys == "Darwin":
         print("Does not work on MAC yet.")
     elif sys == "Linux" or sys == "Unix":
+        subprocess.call("g++ -o run_tests {} -lgtest -lgtest_main -pthread".format(filename))
         if testname is None:
-            process = subprocess.call("./run_tests")
+            process = subprocess.check_output("./run_tests")
         else:
             if subtestname is None:
-                process = subprocess.call("./run_tests --gtest_filter={}.*".format(testname))
+                process = subprocess.check_output("./run_tests --gtest_filter={}.*".format(testname))
             else:
-                process = subprocess.call("./run_tests --gtest_filter={}.{}*".format(testname, subtestname))
+                process = subprocess.check_output("./run_tests --gtest_filter={}.{}*".format(testname, subtestname))
     else:
         print("Does not work on your system.")
+
