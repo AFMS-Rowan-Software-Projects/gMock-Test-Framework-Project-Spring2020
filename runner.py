07654a393a799f6d9b5e96fced9442e1b829996b
nmichelette <43892144+nmichelette@users.noreply.github.com>
2020-03-23 07:16:14 -0400

    maybe finished basic stats

--- runner.py
+++ runner.py
@@ -1,5 +1,11 @@
 import subprocess
 import platform
+import re
+
+
+totalRegex = re.compile(r'[==========] Running [0-9]* tests')
+passedRegex = re.compile(r'[  PASSED  ] [0-9]* tests')
+numRegex = re.compile(r'[0-9]*')
 
 
 def run_tests(filename, testname=None, subtestname=None):
@@ -13,18 +19,25 @@ def run_tests(filename, testname=None, subtestname=None):
     elif sys == "Linux" or sys == "Unix":
         # if the file name has a .cpp, then compile, if not, just run it
         if filename.endswith('.cpp'):
-            subprocess.call(["g++", filename, "-o run_tests", "-lgtest", "-lgtest_main", "-pthread"])
+            subprocess.call(["g++", filename, "-orun_tests", "-lgtest", "-lgtest_main", "-pthread"])
             temp = "run_tests"
         if testname is None:
             process = subprocess.check_output("./{}".format(temp))
+            subprocess.call("./{}".format(temp))
         else:
             if subtestname is None:
                 process = subprocess.check_output(["./{}".format(temp), "--gtest_filter={}.*".format(testname)])
+                subprocess.call(["./{}".format(temp), "--gtest_filter={}.*".format(testname)])
             else:
                 process = subprocess.check_output(["./{}".format(temp), "--gtest_filter={}.{}*".format(testname, subtestname)])
+                subprocess.call(["./{}".format(temp), "--gtest_filter={}.{}*".format(testname, subtestname)])
+        get_basic_stats(process)
     else:
         print("Does not work on your system.")
 
 
 def get_basic_stats(results):
-    
+    total = int(numRegex.search(totalRegex.search(results)))
+    passed = int(numRegex.search(passedRegex.search(results)))
+    percent = (passed/total)*100
+    print("{}/{} ({}%) of the tests passed.".format(passed, total, percent))
