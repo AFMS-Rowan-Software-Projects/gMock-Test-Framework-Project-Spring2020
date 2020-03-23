62f1ee6d118d31e3b0777692711b1d754e211213
nmichelette <43892144+nmichelette@users.noreply.github.com>
2020-03-23 04:53:03 -0400

    attempting to run gtest through python

--- hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	// this is a comment
-	cout << "Hello world!" << endl;
-}
-
--- runner.py
+++ runner.py
@@ -1,8 +1,21 @@
 import subprocess
+import platform
 
 
-print("hello")
 
-process = subprocess.call('hello_world', shell=False)
-
-print(process)
+def run_tests(testname = None, subtestname = None):
+    sys = platform.system()
+    if sys == "Windows":
+        print("Does not work on windows yet.")
+    elif sys == "Darwin":
+        print("Does not work on MAC yet.")
+    elif sys == "Linux" or sys == "Unix":
+        if testname is None:
+            process = subprocess.call("./run_tests")
+        else:
+            if subtestname is None:
+                process = subprocess.call("./run_tests --gtest_filter={}.*".format(testname))
+            else:
+                process = subprocess.call("./run_tests --gtest_filter={}.{}*".format(testname, subtestname))
+    else:
+        print("Does not work on your system.")
