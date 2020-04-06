8689518c645bc29bcbd4e963b7e327224415939d
nmichelette <43892144+nmichelette@users.noreply.github.com>
2020-04-06 14:41:20 -0400

    assert checker

--- personal_tests.py
+++
@@ -1,4 +0,0 @@
-from full_file_creator import find_every_include, make_empty_test, make_full_file
-
-
-make_full_file("geeks.cpp", transfer_class_to_new_file=True)
--- runner.py
+++ runner.py
@@ -1,7 +1,4 @@
-import subprocess
-import platform
-import re
-
+import subprocess, platform, re, configparser
 
 totalRegex = re.compile(r'\[==========\] Running [0-9]+ tests')
 passedRegex = re.compile(r'\[  PASSED  \] [0-9]+ tests')
@@ -12,6 +9,7 @@ def run_tests(filename, testname=None, subtestname=None):
     sys = platform.system()
     process = None
     temp = filename
+    will_run = True
     if sys == "Windows":
         print("Does not work on windows yet.")
     elif sys == "Darwin":
@@ -21,17 +19,19 @@ def run_tests(filename, testname=None, subtestname=None):
         if filename.endswith('.cpp'):
             subprocess.call(["g++", filename, "-orun_tests", "-lgtest", "-lgtest_main", "-pthread"])
             temp = "run_tests"
-        if testname is None:
-            process = subprocess.check_output("./{}".format(temp))
-            subprocess.call("./{}".format(temp))
-        else:
-            if subtestname is None:
-                process = subprocess.check_output(["./{}".format(temp), "--gtest_filter={}.*".format(testname)])
-                subprocess.call(["./{}".format(temp), "--gtest_filter={}.*".format(testname)])
+            will_run = assert_warning(filename, tn=testname, stn=subtestname)
+        if will_run:
+            if testname is None:
+                process = subprocess.check_output("./{}".format(temp))
+                subprocess.call("./{}".format(temp))
             else:
-                process = subprocess.check_output(["./{}".format(temp), "--gtest_filter={}.{}*".format(testname, subtestname)])
-                subprocess.call(["./{}".format(temp), "--gtest_filter={}.{}*".format(testname, subtestname)])
-        get_basic_stats(process)
+                if subtestname is None:
+                    process = subprocess.check_output(["./{}".format(temp), "--gtest_filter={}.*".format(testname)])
+                    subprocess.call(["./{}".format(temp), "--gtest_filter={}.*".format(testname)])
+                else:
+                    process = subprocess.check_output(["./{}".format(temp), "--gtest_filter={}.{}*".format(testname, subtestname)])
+                    subprocess.call(["./{}".format(temp), "--gtest_filter={}.{}*".format(testname, subtestname)])
+            get_basic_stats(process)
     else:
         print("Does not work on your system.")
 
@@ -45,3 +45,20 @@ def get_basic_stats(results):
         percent = (passed / total) * 100
     print("{}/{} ({}%) of the tests passed.".format(passed, total, percent))
 
+
+# Returns a bool, true if the amount of asserts is less than the max asserts
+def assert_warning(fn, tn=r".*", stn=r".*"):
+    f = open(fn)
+    tests = re.findall(r"TEST\s*\(\s*" + tn + r"\s*,\s+" + stn + r"\s*\)\s*{[\s\S]*?}", f.read())
+    f.close()
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    can_run = True
+    for t in tests:
+        length = len(re.findall(r"ASSERT", t))
+        if length > int(config['run_settings']['max_asserts']):
+            print("ERROR: " + re.findall(r"TEST\s*\(\s*.*\)", t)[0] + " has too many asserts, not running tests!", 'red')
+            can_run = False
+        elif length > int(config['run_settings']['assert_warning']):
+            print("WARNING: " + re.findall(r"TEST\s*\(\s*.*\)", t)[0] + " has a lot of asserts.")
+    return can_run
