c8239c279cac2d09f168f95e9a13bc199dbfb5e8
scottzockoll <32425272+scottzockoll@users.noreply.github.com>
2020-04-24 19:43:09 -0400

    Prompt each method of mocked class

--- step_through_format.py
+++ step_through_format.py
@@ -19,6 +19,12 @@ def start_step_through_format(mock_class, methods):
 
     # create a test for each method
     for m in methods:
-        print(m)
+        if input('Do you want to test the method {}? (y/n)'.format(m.name)) == "y":
+            test_name = input("Enter name of test: ")
+            create_test(test_suite_name, test_name)
 
     # end with creating main function and write to file
+
+
+def create_test(test_suite_name, test_name):
+    test_one = MacroFunction('TEST', test_suite_name, test_name)
