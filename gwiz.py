5002d07be206c3c1e33c6d8fba94d07eb663d52d
scottzockoll <32425272+scottzockoll@users.noreply.github.com>
2020-04-24 20:20:44 -0400

    change write to file

--- gwiz.py
+++ gwiz.py
@@ -54,6 +54,6 @@ if args.step:
     parser = parse_cpp_file(file_obj)
     parser._parse_class()
     methods = parser.detect_methods()
-    mock_class = create_mock_class_new(parser.detected_class_name, methods, write_to_disk=False)
+    mock_class = create_mock_class_new(parser.detected_class_name, methods)
     start_step_through_format(parser.detected_class_name, methods)
 
